4f8e8e8f63f8e42c6c27046e621b7875efc8c969
w080by <60482338+w080by@users.noreply.github.com>
2020-04-10 15:17:14 +0300

    Add files via upload

---
+++ scrypt.js
@@ -0,0 +1,11 @@
+function button() {
+  let para = document.createElement('p');
+  para.textContent = 'No, pls!!! Noooooooooooo, Im tickled'
+  document.body.appendChild(para);
+}
+
+const buttons = document.querySelectorAll('button');
+
+for(let i = 0; i < buttons.length ; i++) {
+  buttons[i].addEventListener('click', createParagraph);
+}
